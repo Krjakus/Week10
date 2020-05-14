f2ffc4567a273673cd94664d06d79bd5344018d6
Krjakus <61901841+Krjakus@users.noreply.github.com>
2020-05-14 22:11:19 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+"use strict"
+
+
+
+
+
+/*//2
+let x = String(prompt("say number"));
+console.log(zeros(x,5));
+function zeros(x,i){
+    if(i<=0){return x}
+    return zeros("0"+x,--i);
+}
+ */
+/*//3
+let x="я говорю ей всё спи жена";
+console.log(camelize(x));
+function camelize(str) {
+    return str
+        .split(' ')
+        .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
+        .join('');
+}
+*/
+/*//5
+let arr=[1,5,9,0,3,1,6,7,9,4,5,-5,-9,-7,5,-1,0,2,5,6,5,2];
+    console.log(arr.sort((a, b) => a-b));
+    console.log(arr.filter(function (element) {return element > 0}));
+*/
+
